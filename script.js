67cd0019b9cf72d2f29d54238d028351e1ae52a6
Eliseo <eliseo@Nidias-MacBook-Air.local>
2022-11-15 10:58:08 -0600

    added current day

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ var searchButton = document.getElementById('search-button')
 var input = document.getElementById('input')
 var fiveDay = document.getElementById('five-day')
 var currentDay = document.getElementById('current-Day')
-// var div = document.getElementById('hide')
-// var div2 = document.getElementById('hide2')
-// var div3 = document.getElementById('hide3')
-// var div4 = document.getElementById('hide4')
-// var div5 = document.getElementById('hide5')
 var temp= document.querySelector('.temp')
 var Api = 'http://api.openweathermap.org/data/2.5/forecast'
 var apikey= '3f229a0cbb5a3bd75972e71a79b56b2b'
@@ -27,6 +22,8 @@ searchButton.addEventListener('click',function() {
   .then((data) => { for(var i = 0; i < data.list.length; i+=8){
     console.log(data)
     console.log(data.list[i])
+    var todayTemp = document.getElementById('todays-temp');
+    todayTemp.textContent = data.list[0].main.temp_max;
     var fiveDayCard = ` <div id="day-${i}" class="hide-card">
     <h2 class="city-date-icon">${new Date(data.list[i].dt_txt).toLocaleDateString("en-US")}</h2>
     <p class="temp">temp:${Math.floor(data.list[i].main.temp_max)}</p>
@@ -42,11 +39,6 @@ fiveDay.innerHTML+=fiveDayCard
    
   })
 
-
-  
-
-
-
 });
 
 
@@ -55,14 +47,18 @@ searchButton.addEventListener('click',function() {
 .then((response => response.json()))
 .then((data) => { 
 console.log(data)
-var currentDayCard = ` <div id="current-Day" >
-<h2 class="city-date-icon">${new Date(data.list[0].dt_txt).toLocaleDateString("en-US")}</h2>
-<p id="todays-temp">temp:${Math.floor(data.list[0].main.temp_max)}</p>
-<p id="todays-humidity">humidity:${Math.floor(data.list[0].main.humidity)}</p>
-<p id="todays-wind">wind:${Math.floor(data.list[0].wind.speed)} mph</p>
-</div>
-`
-currentDay.textContent=currentDayCard
+var City = document.getElementById('city');
+City.textContent = data.city.name;
+var todayTemp = document.getElementById('todays-temp');
+todayTemp.textContent = data.list[0].main.temp_max;
+var todaysIcon = document.getElementById('todays-icon')
+todaysIcon.textContent = data.list[0].weather[0].main;
+var todaysHumidity = document.getElementById('todays-humidity')
+todaysHumidity.textContent = data.list[0].main.humidity;
+var todaysWind = document.getElementById('todays-wind')
+todaysWind.textContent = data.list[0].wind.speed;
+
+
 
 
 
